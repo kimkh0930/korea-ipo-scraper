662ad52c2d2d205e1b3325e5e4920a6aa4102d9b
hyungjujeon <gudwn1549@gmail.com>
2021-07-30 23:28:16 +0900

    fix: get_ipo_data_from_38com(), write_on_spreadsheet() 수정
    - 스프레드 시트에 Timestamp, datetime 타입: not JSON serializable 에러를 피하기 위해 일단 string 타입으로 입력
    - Google Sheets Api의 Read & Write request 할당량이 분당 60(사용자), 300 / 일당 10,000 인점을 고려하여 3페이지만 페이지당 60초씩 휴식
    
    TODO:
    - 현재 기능을 초기화 함수로 두고, 일단위로 리프레쉬 할 방법론 찾기
    - json 모듈 미인식 문제 해결하기
    - 지금이라도 작업브랜치 나눠서 정석적으로 작업하기(현재 main 브랜치만 존재)

--- storing/ipo-upcoming-data.py
+++ storing/ipo-upcoming-data.py
@@ -1,8 +1,9 @@
 import requests
-from datetime import datetime, timedelta
-from bs4 import BeautifulSoup
 import pandas as pd
 import gspread
+import time
+from datetime import datetime, timedelta
+from bs4 import BeautifulSoup
 from oauth2client.service_account import ServiceAccountCredentials
 
 class WebInfo:
@@ -13,20 +14,17 @@ class WebInfo:
 
 class IpoData(WebInfo):
     def get_ipo_data(self):
-        total_row_data = []
-        for pg in range(2, self._page+1):
-            full_url = self._url % pg
-            response = requests.get(full_url, headers={'User-Agent': 'Mozilla/5.0'})
-            html = response.text
-            soup = BeautifulSoup(html, 'lxml')
-            data = soup.find('table', {'summary': str(self._table_summary)})
-            pg_row_data = data.find_all('tr')[2:]
-            total_row_data += pg_row_data
+        full_url = self._url % self._page
+        response = requests.get(full_url, headers={'User-Agent': 'Mozilla/5.0'})
+        html = response.text
+        soup = BeautifulSoup(html, 'lxml')
+        data = soup.find('table', {'summary': str(self._table_summary)})
+        pg_row_data = data.find_all('tr')[2:]
 
-        return total_row_data
+        return pg_row_data
 
-def get_ipo_data_from_38com():
-    schedule_web_info = WebInfo('http://www.38.co.kr/html/fund/index.htm?o=k&page=%s', 1, '공모주 청약일정')
+def get_ipo_data_from_38com(url, page, summary):
+    schedule_web_info = WebInfo(url, page, summary)
     temp_ipo_data_schedule = IpoData(schedule_web_info._url, schedule_web_info._page, schedule_web_info._table_summary)
     total_row_data_schedule = temp_ipo_data_schedule.get_ipo_data()
 
@@ -46,9 +44,13 @@ def get_ipo_data_from_38com():
         for j, uw in enumerate(underwriter):
             company_list.append(data_list[0].replace('(유가)', ''))
 
-            sub_start_date = datetime.strptime(data_list[1][:10], '%Y.%m.%d')
+            sub_start_date = str(datetime.strptime(data_list[1][:10], '%Y.%m.%d'))[0:10]
             sub_fin_date = datetime.strptime(data_list[1][:5] + data_list[1][-5:], '%Y.%m.%d')
             refund_date = sub_fin_date + timedelta(days=4) if sub_fin_date.weekday() > 4 else sub_fin_date + timedelta(days=2)
+
+            # TypeError: Object of type Timestamp/datetime is not JSON serializable -> type change to str
+            sub_fin_date = (str(sub_fin_date))[0:10]
+            refund_date = str(refund_date)[0:10]
 
             subscription_start_list.append(sub_start_date)
             subscription_finish_list.append(sub_fin_date)
@@ -92,11 +94,21 @@ def write_on_spreadsheet(spreadsheet, data):
     for row in data.to_numpy():
         newrow = [''] + row.tolist() + ['', ''] #신규 상장일, 기관 경쟁률 및 의무보유 확약 빈칸
         print(newrow)
-        # TypeError: Object of type Timestamp is not JSON serializable
-        newrow[2:5] = [newrow[2].to_pydatetime(), newrow[3].to_pydatetime(), newrow[4].to_pydatetime()]
         spreadsheet.append_row(newrow)
 
-ipo_upcoming_data = get_ipo_data_from_38com()
-ipo_spreadsheet = get_google_spreadsheet('ipo_participated')
+def main_function():
+    target_url = 'http://www.38.co.kr/html/fund/index.htm?o=k&page=%s'
+    page_to_crawl = 3
+    target_table_summary = '공모주 청약일정'
+    target_web = WebInfo(target_url, page_to_crawl, target_table_summary)
 
-write_on_spreadsheet(ipo_spreadsheet, ipo_upcoming_data)
+    for page in range(target_web._page, 0, -1):
+        ipo_upcoming_data = get_ipo_data_from_38com(target_web._url, page, target_web._table_summary)
+        ipo_spreadsheet = get_google_spreadsheet('ipo_participated')
+        write_on_spreadsheet(ipo_spreadsheet, ipo_upcoming_data)
+
+        #Allowed read & write requests per min : 60 requests
+        time.sleep(60)
+
+if __name__ == '__main__':
+    main_function()
